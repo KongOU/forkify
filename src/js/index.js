3c024afeb82295637b5d78ce7941fd71aa5675fb
kongou <oukong84@gmail.com>
2020-02-14 12:16:17 +0700

    Clear git cache

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
-// Global app controller
-import num from './test';
-const x = 23;
+import axios from 'axios';
 
-console.log(`I imported ${num} from another module call test.js! Variable x is ${x}`);
+async function getResults(query) {
+  // const proxy = 'https://cors-anywhere.herokuapp.com/';
+  // const key = '';
+  try {
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    alert(error);
+  }
+}
+getResults('curry');
