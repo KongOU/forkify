80b8d98d00e06e5162d769e8577561b1a87d6214
kongou <oukong84@gmail.com>
2020-02-18 23:36:51 +0700

    Building the Shopping List

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
@@ -78,7 +79,6 @@ const controlRecipe = async () => {
     try {
       // Get Recipe data and parse ingredients
       await state.recipe.getRecipe();
-      console.log(state.recipe.ingredients);
       state.recipe.parseIngredient();
 
       // Calculate serving and time
@@ -109,5 +109,6 @@ elements.recipe.addEventListener('click', e => {
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
   }
-  console.log(state.recipe);
 });
+
+window.l = new List();
