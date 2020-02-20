8c1f6acde9498cdb6ce4fc3b9a6c5a6add6a91a4
kongou <oukong84@gmail.com>
2020-02-20 20:50:48 +0700

    Building the Likes View and Fix bugs

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
+import * as likesView from './views/likesView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -66,7 +67,7 @@ elements.searchResultPages.addEventListener('click', e => {
 const controlRecipe = async () => {
   // Get ID from url
   const id = window.location.hash.replace('#', '');
-  // console.log(id);
+  console.log(id);
 
   if (id) {
     // Prepare UI for changes
@@ -90,8 +91,9 @@ const controlRecipe = async () => {
 
       // Render recipe
       clearLoader();
-      recipeView.renderRecipe(state.recipe);
+      recipeView.renderRecipe(state.recipe, state.likes.isLiked(id));
     } catch (error) {
+      console.log(error);
       alert('Error processing recipe!');
     }
   }
@@ -135,6 +137,9 @@ elements.shopping.addEventListener('click', e => {
 /**
  * LIKE CONTROLLER
  */
+state.likes = new Likes();
+likesView.toggleLikeMenu(state.likes.getNumLikes());
+
 const controlLike = () => {
   if (!state.likes) state.likes = new Likes();
 
@@ -146,9 +151,10 @@ const controlLike = () => {
     const newLike = state.likes.addLike(currentID, state.recipe.title, state.recipe.author, state.recipe.img);
 
     // Toggle the like button
+    likesView.toggleLikeBtn(true);
 
     // Add like to UI list
-    console.log(state.likes);
+    likesView.renderLike(newLike);
 
     // User HAS liked current recipe
   } else {
@@ -156,10 +162,12 @@ const controlLike = () => {
     state.likes.deleteLike(currentID);
 
     // Toggle the like button
+    likesView.toggleLikeBtn(false);
 
     // Remove like from UI list
-    console.log(state.likes);
+    likesView.deleteLike(currentID);
   }
+  likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
 
 // Handling recipe button click
--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -20,5 +20,5 @@ export const renderItem = item => {
 
 export const deleteItem = id => {
   const item = document.querySelector(`[data-itemid="${id}"]`);
-  item.parentElement.removeChild(item);
+  if (item) item.parentElement.removeChild(item);
 };
